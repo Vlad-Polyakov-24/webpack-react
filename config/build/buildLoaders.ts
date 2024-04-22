b7e31449f04bc65226c176539167e1efd074ca06
Vladyslav Poliakov <vladon24121995@gmail.com>
2024-04-22 17:07:57 +0300

    feat: babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,10 +3,6 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
 export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => {
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    };
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
@@ -14,6 +10,25 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
                 loader: 'file-loader',
             },
         ],
+    };
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    };
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            },
+        },
+    };
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     };
     const cssLoader = {
         test: /\.s[ac]ss$/i,
@@ -38,15 +53,11 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
             'sass-loader',
         ],
     };
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
 
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ];
